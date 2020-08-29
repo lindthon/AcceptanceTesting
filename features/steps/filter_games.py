84e1f95df56a360e79e3ac907cf31eee0c75eace
lindthon <43838005+lindthon@users.noreply.github.com>
2020-08-28 22:58:24 -0500

    Report added

--- features/steps/filter_games.py
+++ features/steps/filter_games.py
@@ -55,3 +55,74 @@ def step_impl(context, message):
 	print(message)
 	print(context.message)
 	assert context.message == message
+
+	@given("the user selects one or more ratings: {rating}")
+def step_impl(context, rating):
+	context.rating = rating.split(",")
+
+@when("choosing the search by ratings {option}")
+def step_impl(context, option):
+	if(option == "name"):
+		r, message = get_game_name(context.games, context.name)
+		print(r)
+		context.r=r
+		context.message=message
+	elif(option == "rating"):
+		r,message, err=get_game_rating(context.games, context.rating)
+		print(r)
+		context.r=r
+		context.message=message
+
+@then("the name of all the games that correspond to these categories will be displayed: {message}")
+def step_impl(context, message):
+	print(message)
+	print(context.message)
+	assert context.message == message
+"""
+@given("the user selects one or more ratings: {rating1}")
+def step_impl(context, rating1):
+	context.rating = rating1.split(",")
+
+@when("choosing the no match ratings {option1}")
+def step_impl(context, option1):
+	if(option1 == "name"):
+		r, message = get_game_name(context.games, context.name)
+		print(r)
+		context.r=r
+		context.message=message
+	elif(option1 == "rating"):
+		r,message, err=get_game_rating(context.games, context.rating)
+		print(r)
+		context.r=r
+		context.message=message"""
+
+@then("No game with the specified rating was found: {message}")
+def step_impl(context, message):
+	print(message)
+	print(context.message)
+	assert context.message == message
+
+
+@given("the user enters a studio: {studio}")
+def step_impl(context, studio):
+	context.rating = studio
+
+@when("they select the search by studio {option}")
+def step_impl(context, option):
+	if(option == "name"):
+		r, message = get_game_name(context.games, context.name)
+		print(r)
+		context.r=r
+		context.message=message
+	elif(option == "rating"):
+		r,message, err=get_game_rating(context.games, context.rating)
+		print(r)
+		context.r=r
+		context.message=message
+
+@then("the name of all the games that have been developed by that studio will be displayed: {message}")
+def step_impl(context, message):
+	print(message)
+	print(context.message)
+	assert context.message == message
+
